88f574934d62fddb2c315c7b34ac4326af562c85
ash-miku <29676159+ash-miku@users.noreply.github.com>
2020-06-13 16:54:43 +0800

    Add files via upload

--- 爬虫/biliTest.py
+++ 爬虫/biliTest.py
@@ -4,64 +4,154 @@ import time
 import os
 import random
 import re
+import pymysql
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 
 def bilibili_Video():
     # 获取网页
-    id = input("请输入爬取视频数量:")
+    numid = input("请输入爬取视频数量:")
     # num = 0
+    success_num = 0
+    fail_num = 0
+
     headers = {
+        'Connection': 'close',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
     # 创建保存路径
     if not os.path.exists('biliVideo'):
         os.mkdir('biliVideo')
+    elif os.path.exists('biliVideo'):
+        try:
+            os.remove('biliVideo/biliVideo_' + str(numid) + '.csv')
+        except:
+            print()
+            print("删除的文件不存在")
+            print()
+        else:
+            print()
+            print("删除文件成功")
+            print()
 
     # 提前打开文件创建列名
-    with open("biliVideo/biliVideo_" + str(id) + ".csv", "a", newline='') as f:
+    with open("biliVideo/biliVideo_" + str(numid) + ".csv", "a", encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(["视频av号", "视频bv号", "观看次数", "弹幕数",
+        writer.writerow(["视频av号", "视频bv号", "视频分区", "视频标题", "作者昵称", "作者UID", "视频通过审核时间", "观看次数", "弹幕数",
                          "收藏数", "投币数", "分享数", "点赞数"])
 
-    for i in range(0, int(id)):
+    for i in range(0, int(numid)):
         num = random.randint(0, 100000000)
-        urls = "https://api.bilibili.com/x/web-interface/archive/stat?aid=" + \
+        urls = "http://api.bilibili.com/x/web-interface/view?aid=" + \
             str(num)
-        # {"code":0,"message":"0","ttl":1,"data":{"aid":540931451,"bvid":"BV1Ri4y1s7Am","view":204266,"danmaku":377,"reply":1452,"favorite":385,"coin":102,"share":950,"like":12343,"now_rank":0,"his_rank":0,"no_reprint":1,"copyright":1,"argue_msg":"","evaluation":""}}
         r = requests.get(urls, headers=headers)
         json = r.json()
         code = json['code']
         if code == 0:
-            data = json['data']  # 拿出子字典
+            data = json['data']  # 拿出data子字典
+            stat = data['stat']  # 拿出stat子字典
+            owner = data['owner']   # 拿出owner子字典
             # 通过字典获取各数据
             list = []
-            aid = str(data['aid'])  # 视频av号
+            aid = str(stat['aid'])  # 视频av号
             list.append(aid)
             bvid = str(data['bvid'])  # 视频bv号
             list.append(bvid)
-            view = str(data['view'])  # 观看次数
+            tid = data['tid']  # 视频分区id
+            if tid in [1, 24, 25, 47, 86, 27]:
+                tname = "动画"
+            elif tid in [13, 33, 32, 51, 152]:
+                tname = "番剧"
+            elif tid in [167, 153, 168, 169, 195, 170]:
+                tname = "国创"
+            elif tid in [3, 28, 31, 30, 194, 59, 193, 29, 130]:
+                tname = "音乐"
+            elif tid in [129, 20, 198, 199, 200, 154, 156]:
+                tname = "舞蹈"
+            elif tid in [4, 17, 171, 172, 65, 173, 121, 136, 19]:
+                tname = "游戏"
+            elif tid in [36, 201, 124, 207, 208, 209, 122]:
+                tname = "知识"
+            elif tid in [188, 95, 189, 190, 191]:
+                tname = "数码"
+            elif tid in [160, 138, 21, 76, 75, 161, 162, 163, 176, 174]:
+                tname = "生活"
+            elif tid in [119, 22, 26, 126, 127]:
+                tname = "鬼畜"
+            elif tid in [155, 157, 158, 164, 159, 192]:
+                tname = "时尚"
+            elif tid in [202, 203, 204, 205, 206]:
+                tname = "资讯"
+            elif tid in [5, 71, 137, 131]:
+                tname = "娱乐"
+            elif tid in [181, 182, 183, 85, 184]:
+                tname = "影视"
+            elif tid in [177, 37, 178, 179, 180]:
+                tname = "纪录片"
+            elif tid in [23, 147, 145, 146, 83]:
+                tname = "电影"
+            elif tid in [11, 185, 187]:
+                tname = "电视剧"
+            list.append(tname)
+            title = str(data['title'])  # 视频标题
+            list.append(title)
+            name = str(owner['name'])   # 视频作者
+            list.append(name)
+            mid = str(owner['mid'])  # 作者UID
+            list.append(mid)
+            ctime = int(data['ctime'])  # 视频通过审核时间
+
+            # 时间戳转换
+            ctimeArray = time.localtime(ctime)
+            ctimeStyleTime = time.strftime("%Y-%m-%d-%H:%M:%S", ctimeArray)
+
+            list.append(ctimeStyleTime)
+            view = str(stat['view'])  # 观看次数
             list.append(view)
-            danmaku = str(data['danmaku'])  # 弹幕数
+            danmaku = str(stat['danmaku'])  # 弹幕数
             list.append(danmaku)
-            favorite = str(data['favorite'])  # 收藏数
+            favorite = str(stat['favorite'])  # 收藏数
             list.append(favorite)
-            coin = str(data['coin'])  # 投币数
+            coin = str(stat['coin'])  # 投币数
             list.append(coin)
-            share = str(data['share'])  # 分享数
+            share = str(stat['share'])  # 分享数
             list.append(share)
-            like = str(data['like'])  # 点赞数
+            like = str(stat['like'])  # 点赞数
             list.append(like)
             # print(aid,bvid,view,danmaku,like,coin,favorite,share)
             # print(type(bvid))
             print(list)
 
-            with open("biliVideo/biliVideo_" + str(id) + ".csv", "a", newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(list)
-                print("写入成功")
+            # 将数据写入本地csv文件
+            # with open("biliVideo/biliVideo_" + str(id) + ".csv", "a", encoding='utf-8', newline='') as f:
+            #     writer = csv.writer(f)
+            #     writer.writerow(list)
+            #     print("写入成功")
+
+            # 将数据写入云数据库
+            try:
+                connent = pymysql.connect(host='miku-miku.top', port=3306,
+                                          user='root', passwd='ly231930', db='bilibili', charset='utf8')
+                cursor = connent.cursor()
+                sql = "replace into bilibili_Video values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (str(list[0]), str(list[1]), str(list[2]), pymysql.escape_string(str(
+                    list[3])), pymysql.escape_string(str(list[4])), str(list[5]), str(list[6]), str(list[7]), str(list[8]), str(list[9]), str(list[10]), str(list[11]), str(list[12]))
+                # print(sql)
+                cursor.execute(sql)
+                connent.commit()
+                cursor.close()
+                connent.close()
+                success_num += 1
+                print()
+                print("数据库写入成功,已完成{0}/{1}......{2:.2%}".format(success_num,int(numid),int(success_num)/int(numid)))
+            except:
+                print("写入失败")
+                fail_num += 1
         else:
             print()
             print("视频不存在！")
+            success_num += 1
             print()
 
         r.close()
@@ -70,6 +160,7 @@ def bilibili_Video():
         # time.sleep(random.randint(2, 4))
     print()
     print("Complete !")
+    print("数据库写入失败次数 : %d" % fail_num)
 
 
 def bilibili_Album():
@@ -99,8 +190,8 @@ def bilibili_Album():
         num = num + 1
         r = requests.get(url)
         newurl = r.text.replace('\\', '')
-        # 解析网页
 
+        # 解析网页
         urls = re.findall(
             'http[s]?://(?:(?!http[s]?://)[a-zA-Z]|[0-9]|[$\-_@.&+/]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
             newurl)
@@ -161,6 +252,16 @@ def bilibili_Info():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
+    # 创建保存路径
+    if not os.path.exists('biliInfo'):
+        os.mkdir('biliInfo')
+
+    # 提前打开文件创建列名
+        with open("biliInfo/biliInfo.csv", "a", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["UID", "昵称", "性别", "个性签名",
+                             "账号等级", "生日", "bilibili认证", "认证标题", "会员类型", "会员状态"])
+
     urls = "https://api.bilibili.com/x/space/acc/info?mid="+str(uid)
     r = requests.get(urls, headers=headers)
     json = r.json()
@@ -196,6 +297,10 @@ def bilibili_Info():
         else:
             role = "bilibili企业认证"
 
+        # 认证标题
+        if title == "":
+            title = "无"
+
         # bilibili会员类型
         if vipType == "0":
             vipType = "普通会员"
@@ -210,6 +315,24 @@ def bilibili_Info():
         elif vipStatus == "1":
             vipStatus = "是"
 
+        # 创建写入列表
+        list = []
+        list.append(mid)
+        list.append(name)
+        list.append(sex)
+        list.append(sign)
+        list.append(level)
+        list.append(birthday)
+        list.append(role)
+        list.append(title)
+        list.append(vipType)
+        list.append(vipStatus)
+
+        # 文件写入
+        with open("biliInfo/biliInfo.csv", "a+", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(list)
+
         print("查询成功！")
         print()
         print("UID : "+mid)
@@ -218,7 +341,7 @@ def bilibili_Info():
         print("个性签名 : "+sign)
         print("账号等级 : "+level)
         print("生日 : "+birthday)
-        print("bilibili认证 : "+role+" "+title)
+        print("bilibili认证 : "+role)
         print("会员类型 : "+vipType)
         print("会员状态 : "+vipStatus)
         print()
@@ -227,14 +350,191 @@ def bilibili_Info():
         print("用户不存在！")
         print()
 
+def bilibili_Chart():
+
+    headers = {
+            'Connection': 'close',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
+
+    rid1 = [24, 25, 47, 86, 27]
+    nums = []
+    num1 = 0
+    for test in rid1:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num1 += count
+    nums.append(num1)
+
+    rid2 = [157, 158, 164, 159, 192]
+    num2 = 0
+    for test in rid2:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num2 += count
+    nums.append(num2)
+
+    rid3 = [71, 137, 131]
+    num3 = 0
+    for test in rid3:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num3 += count
+    nums.append(num3)
+
+    rid4 = [28, 31, 30, 194, 59, 193, 29, 130]
+    num4 = 0
+    for test in rid4:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num4 += count
+    nums.append(num4)
+
+    rid5 = [20, 198, 199, 200, 154, 156]
+    num5 = 0
+    for test in rid5:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num5 += count
+    nums.append(num5)
+
+    rid6 = [17, 171, 172, 65, 173, 121, 136, 19]
+    num6 = 0
+    for test in rid6:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num6 += count
+    nums.append(num6)
+
+    rid7 = [201, 124, 207, 208, 209, 122]
+    num7 = 0
+    for test in rid7:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num7 += count
+    nums.append(num7)
+
+    rid8 = [95, 189, 190, 191]
+    num8 = 0
+    for test in rid8:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num8 += count
+    nums.append(num8)
+
+    rid9 = [138, 21, 76, 75, 161, 162, 163, 176, 174]
+    num9 = 0
+    for test in rid9:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num9 += count
+    nums.append(num9)
+
+    rid10 = [182, 183, 85, 184]
+    num10 = 0
+    for test in rid10:
+        url = "http://api.bilibili.com/x/web-interface/newlist?rid="+str(test)+"&pn=1&ps=1"
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        code = json['code']
+        if code == 0:
+            data = json['data']  # 拿出data子字典
+            page = data['page']
+            count = page['count']
+            num10 += count
+    nums.append(num10)
+    
+    print(nums)
+    #画板设置
+    large=22;med=16;small=12;
+    params = {'axes.titlesize': large,
+            'legend.fontsize': med,
+            'figure.figsize': (12, 6),
+            'axes.labelsize': med,
+            'xtick.labelsize': med,
+            'ytick.labelsize': med,
+            'figure.titlesize': large}
+    plt.rcParams.update(params)
+    plt.style.use('seaborn-whitegrid')
+
+    pop_agekind = ["动画","时尚","娱乐","音乐","舞蹈","游戏","知识","数码","生活","影视"]#数据种类
+
+    plt.axes(aspect=1)#椭圆形状
+    plt.pie(x=nums,labels=pop_agekind,autopct='%3.1f %%')#数据类型
+    #标题设置
+    plt.title("BILIBILI(B站)各分区视频比例")
+    plt.rcParams['font.sans-serif']=['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    #标签设置
+    plt.legend(loc="lower left",fontsize=12,bbox_to_anchor=(1.3,0.4),borderaxespad=0.3)
+    # loc =  'upper right' 位于右上角
+    # bbox_to_anchor=[0.5, 0.5] # 外边距 上边 右边，精准位置
+    # ncol=2 分两列
+    # borderaxespad = 0.3图例的内边距
+    plt.show()
+
 
 if __name__ == '__main__':
-    while 1:
+    while True:
         print("欢迎打开新世界的大门！")
         print()
         print("1.随机爬取B站视频数据")
         print("2.爬取某UP主相簿照片")
         print("3.查询UP主基本信息")
+        print("4.查询各分区视频比例")
         print()
         print("0.退出程序")
         print()
@@ -247,6 +547,10 @@ if __name__ == '__main__':
             break
         elif switch_num == "3":
             bilibili_Info()
+            break
+        elif switch_num == "4":
+            print("正在查询,请稍后...")
+            bilibili_Chart()
             break
         elif switch_num == "0":
             print("Bye~")
